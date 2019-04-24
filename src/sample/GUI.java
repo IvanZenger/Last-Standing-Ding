73472eb0b63134ea6993b61d9ce22e32d7fc7a32
zurbrueggn <nicolazurbruegg@gmail.com>
2019-04-24 09:08:56 +0200

    Draw Line, after 2 iteration the programm is throwing exceptions.

--- src/sample/GUI.java
+++ src/sample/GUI.java
@@ -1,19 +1,123 @@
 package sample;
 
-import javafx.animation.TranslateTransition;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.scene.canvas.Canvas;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+
+public class GUI extends Application {
+
+    private double from_x = 100;
+    private double from_y = 100;
+    private double to_x = 101;
+    private double to_y = 100;
+    private int line_no = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        StackPane root = new StackPane();
+        Canvas canvas = new Canvas(600, 600);
+
+
+        root.getChildren().addAll(new Canvas(), canvas);
+
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setTitle("Last Standing Ding");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+
+   /*     canvas.setOnMousePressed((event) -> setFromPos(event));
+
+
+        canvas.setOnMouseReleased((event) -> {
+            Canvas new_line = new Canvas(600, 600);
+            GraphicsContext gc = new_line.getGraphicsContext2D();
+            setToPos(event);
+            drawLine(gc);
+            //final new stright line
+            root.getChildren().add(++line_no, new_line);
+        });
+*/
+    canvas.setOnMousePressed(event -> {
+        Canvas new_line = new Canvas(600, 600);
+        GraphicsContext gc = new_line.getGraphicsContext2D();
+        for (int i = 0; i < 400; i++){
+
+         //   System.out.println("hi");
+            from_x+=1;
+            to_x+=1;
+          //  setToPos(event);
+            System.out.println(from_x + " " + to_x);
+            drawLine(gc);
+            root.getChildren().add(line_no, new_line);
+
+        }
+    });
+
+
+
+
+
+
+                /*canvas.setOnMouseDragged((event) -> {
+            System.out.println("Test");
+            root.getChildren().remove(0);
+            Canvas temp_canvas = new Canvas(600, 600);
+            GraphicsContext gc = temp_canvas.getGraphicsContext2D();
+            this.setToPos(event);
+            this.drawLine(gc);
+            root.getChildren().add(0, temp_canvas);
+        });*/
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void drawLine(GraphicsContext gc) {
+       // gc.setFill(Color.RED);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(from_x, from_y, to_x, to_y);
+    }
+
+    private void setFromPos(MouseEvent event) {
+        from_x = event.getSceneX();
+        from_y = event.getSceneY();
+
+    }
+
+    private void setToPos(MouseEvent event) {
+        to_x = event.getSceneX();
+        to_y = event.getSceneY();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 public class GUI extends Application {
     @Override
     public void start(Stage stage) {
@@ -35,6 +139,12 @@ public class GUI extends Application {
         trans.setAutoReverse(true);
         trans.setNode(line);
         trans.play();
+
+
+
+
+
+
 
 
 
@@ -101,3 +211,7 @@ public class GUI extends Application {
         launch(args);
     }
 }
+
+
+
+ */
