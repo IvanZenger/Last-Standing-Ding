32e15e86d45a5982d26894e7dc14808cae035ea4
Nicola Zurbrügg <nicola.zurbruegg@ict-campus.net>
2019-04-25 15:51:16 +0200

    working at the detection

--- src/sample/GUI.java
+++ src/sample/GUI.java
@@ -1,26 +1,18 @@
 package sample;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import javax.imageio.ImageIO;
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Type;
 
 import static java.lang.Math.PI;
 
@@ -33,11 +25,12 @@ public class GUI extends Application implements Runnable{
     private int line_no = 0;
     private double angle = 90;
     private double speed = 2;
+    private int iterations = 0;
     private String direction = "NONE";
     private Canvas canvas = new Canvas(600, 600);
     private Canvas new_line = new Canvas(600, 600);
     private GraphicsContext gc = new_line.getGraphicsContext2D();
-
+    private StackPane root = new StackPane();
 
     private AnimationTimer timer = new AnimationTimer() {
         @Override
@@ -58,7 +51,7 @@ public class GUI extends Application implements Runnable{
     public void start(Stage primaryStage) {
 
         canvas.setFocusTraversable(true); //Damit die KeyInputs registriert wereden
-        StackPane root = new StackPane();
+
         root.getChildren().addAll(new Canvas(), canvas);
 
         Scene scene = new Scene(root, 600, 600);
@@ -103,9 +96,26 @@ public class GUI extends Application implements Runnable{
     }
 
     private void update(GraphicsContext gc){ //Bei jedem Timer Tick wird diese Methode ausgeführt
-
+        Color color;
+        iterations+=1;
         updateAngle();
-        drawLine(gc); //Linie zeichnen bzw. updaten.
+        calculateNewCoordinates();
+        checkOnCrash(); //es wird geprüft ob sich der Spieler auserhalb des spieles und oder sich auf einer anderen Linie befindet.
+        if (iterations % 140 >= 0 && iterations % 140 < 20){
+            color = new Color(0,0,1,1);
+            drawCirlce(color);
+        }
+        else{
+            color = new Color(0,0,1,1);
+            drawLine(gc, color); //Linie zeichnen bzw. updaten.
+        }
+       // System.out.println(iterations % 5000);
+
+
+
+
+        from_x = to_x; //neue Anfangskoorinaten festlegen
+        from_y = to_y;
         try {
             Thread.sleep(1); //geschwindigkeit der Linie regulieren
         }
@@ -113,24 +123,33 @@ public class GUI extends Application implements Runnable{
             e.printStackTrace();
         }
     }
+
 /**
  * @author Nicola Zurbügg / zurbrueggn / NiciAlmighty
  * Diese Methode zeichnet mithilfe des speeds und des Winkels die nächste linie aus und zeichnet diese.
  * @version 1.0.0
  */
-    public void drawLine(GraphicsContext gc) {
-        gc.setStroke(Color.BLUE);
+    public void drawLine(GraphicsContext gc, Color color) {
+        gc.setStroke(color);
         gc.setLineWidth(3);
-
-        // (angle/360)*(2*PI) --damit man den Winkel in Grad angeben kann ansonsten müsste man 2pi als 360Grad angeben.
-        to_x = from_x + Math.sin((angle/360)*(2*PI))*speed; //neue X koordinate in Abhängigkeit vom Winkel berechnen
-        to_y = from_y + Math.cos((angle/360)*(2*PI))*speed; //neue Y koordinate in Abhängigkeit vom Winkel berechnen
-        checkOnCrash(); //es wird geprüft ob sich der Spieler auserhalb des spieles und oder sich auf einer anderen Linie befindet.
         gc.strokeLine(from_x, from_y, to_x, to_y); //neue Linie zeichnen
-
-        from_x = to_x; //neue Anfangskoorinaten festlegen
-        from_y = to_y;
     }
+
+
+    public void drawCirlce(Color color){
+        Circle circle = new Circle(to_x, to_y, 4, color);
+
+        root.getChildren().addAll(circle);
+    }
+
+public void calculateNewCoordinates(){
+    // (angle/360)*(2*PI) --damit man den Winkel in Grad angeben kann ansonsten müsste man 2pi als 360Grad angeben.
+    to_x = from_x + Math.sin((angle/360)*(2*PI))*speed; //neue X koordinate in Abhängigkeit vom Winkel berechnen
+    to_y = from_y + Math.cos((angle/360)*(2*PI))*speed; //neue Y koordinate in Abhängigkeit vom Winkel berechnen
+}
+
+
+
 
     /**
      * @author Nicola Zurbügg / zurbrueggn / NiciAlmighty
@@ -140,24 +159,19 @@ public class GUI extends Application implements Runnable{
      */
     public void checkOnCrash(){
 
-        WritableImage writableImage = new_line.snapshot(null, null);
-        //WritableImage writableImage = new WritableImage((int) new_line.getWidth(), (int) new_line.getHeight());
-        // System.out.print(writableImage.getPixelReader().getColor((int) to_x, (int)to_y));
-        Color x = writableImage.getPixelReader().getColor((int) to_x, (int)to_y);
-      //  System.out.println(x);
+       // WritableImage writableImage = new_line.snapshot(null, null);
+       // Color x = writableImage.getPixelReader().getColor((int) to_x, (int)to_y);
+       // System.out.println(x);
+        // || x.equals("0x0000ffff")
 
-      //  System.out.println(Integer.toHexString(bi.getRGB(50, 550)));
-        if (600 < to_x || 0 > to_x || 600 < to_y || 0 > to_y || x.equals("0x0000ffff")){
+        if (to_x >= 600 || to_x <= 0|| to_y >= 600 || to_y <= 0){
             timer.stop();
+            System.out.println(to_x + " " + to_y);
+
         }
-
-
       //  System.out.println(x.getRed() + " " +x.getGreen() + " " + x.getBlue());
-
-
-
-
     }
+
     public void updateAngle(){
         if (direction == "LEFT"){
             angle += 6; //Winkel der linie wird um 8 Grad erhöht
